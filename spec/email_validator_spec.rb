a58ce87c4a80c64524f4ce75cfdfe01ea447b6b0
Tim Morgan <git@timothymorgan.info>
2022-09-18 12:31:19 -0700

    Spec updates

--- spec/email_validator_spec.rb
+++ spec/email_validator_spec.rb
@@ -6,25 +6,25 @@ describe EmailValidator do
   end
   
   it "should validate an email address" do
-    @validator.valid?(nil, nil, 'foo@bar.com').should be_true
+    expect(@validator.valid?(nil, nil, 'foo@bar.com')).to be_true
   end
   
   it "should validate a name and email pair" do
-    @validator.valid?(nil, nil, 'Foo Bar <foo@bar.com>').should be_true
+    expect(@validator.valid?(nil, nil, 'Foo Bar <foo@bar.com>')).to be_true
   end
   
   it "should not validate an improper email address" do
-    @validator.valid?(nil, nil, 'foo@bar').should be_false
-    @validator.valid?(nil, nil, 'fo oo@bar.com').should be_false
-    @validator.valid?(nil, nil, 'foo@bar@bar.com').should be_false
-    @validator.valid?(nil, nil, 'foo@.com').should be_false
+    expect(@validator.valid?(nil, nil, 'foo@bar')).to be_false
+    expect(@validator.valid?(nil, nil, 'fo oo@bar.com')).to be_false
+    expect(@validator.valid?(nil, nil, 'foo@bar@bar.com')).to be_false
+    expect(@validator.valid?(nil, nil, 'foo@.com')).to be_false
   end
   
   it "should not validate an improper name and email pair" do
-    @validator.valid?(nil, nil, 'Foo Bar <foo@bar>').should be_false
-    @validator.valid?(nil, nil, 'Foo Bar <fo oo@bar.com>').should be_false
-    @validator.valid?(nil, nil, 'Foo Bar <foo@bar@bar.com>').should be_false
-    @validator.valid?(nil, nil, 'Foo Bar <foo@.com>').should be_false
-    @validator.valid?(nil, nil, '<foo@bar.com> Foo Bar').should be_false
+    expect(@validator.valid?(nil, nil, 'Foo Bar <foo@bar>')).to be_false
+    expect(@validator.valid?(nil, nil, 'Foo Bar <fo oo@bar.com>')).to be_false
+    expect(@validator.valid?(nil, nil, 'Foo Bar <foo@bar@bar.com>')).to be_false
+    expect(@validator.valid?(nil, nil, 'Foo Bar <foo@.com>')).to be_false
+    expect(@validator.valid?(nil, nil, '<foo@bar.com> Foo Bar')).to be_false
   end
 end
